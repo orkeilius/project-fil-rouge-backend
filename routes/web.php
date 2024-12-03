052aa6ea08a29e81d36881b98c46eb3549b19326
EnzoColantonio34 <enzo.colantonio@sfr.fr>
2024-12-03 11:53:32 +0000

    Users OK

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
+Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+Route::post('/users', [UserController::class, 'store'])->name('users.store');
+*/
+
+Route::resource('users', UserController::class);
