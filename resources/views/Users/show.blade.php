c4c314dba0d7f997fcfbb5fd1c3197ceed41e646
EnzoColantonio34 <enzo.colantonio@sfr.fr>
2025-01-27 08:38:46 +0000

    add css style

--- resources/views/Users/show.blade.php
+++ resources/views/Users/show.blade.php
@@ -5,6 +5,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Liste des utilisateurs</title>
 </head>
 <body>
