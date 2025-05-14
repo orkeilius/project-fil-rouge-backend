43221987b3aefbb5611050f8f05e99bbf0dacfc7
Matthieu D. <orkeilius@gmail.com>
2025-05-14 10:44:31 +0200

    add filter in getAuction from user

--- app/Http/Controllers/api/v1/UserApiController.php
+++ app/Http/Controllers/api/v1/UserApiController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\api\v1;
 
+use App\Http\Requests\Auction\AuctionFilteredIndexRequest;
 use App\Http\Requests\User\UserDestroyRequest;
 use App\Http\Requests\User\UserStoreRequest;
 use App\Mail\WelcomeMail;
+use App\Models\Auction;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
@@ -32,13 +34,37 @@ class UserApiController extends Controller
     public function showSelf()
     {
         $user = User::findOrFail(auth()->id());
-        return response()->json($user); // Retourne l'utilisateur en JSON
+        // Calculer les statistiques
+        $stats = [
+            'auctions_ongoing' => $user->Auctions()->where('end_at', '>', now())->count(),
+            'auctions_ended' => $user->Auctions()->where('end_at', '<', now())->count(),
+            'auctions_won' => Auction::whereHas('offers', function($query) use ($user) {
+                $query->where('author_id', $user->id)
+                    ->whereRaw('offers.id = (SELECT MAX(o.id) FROM offers o WHERE o.auction_id = auctions.id)');
+            })->where('end_at', '<', now())->count()
+        ];
+
+        // Ajouter les statistiques à la réponse
+        $userData = $user->toArray();
+        $userData['stats'] = $stats;
+        return response()->json($userData); // Retourne l'utilisateur en JSON
     }
-    public function getAuctions($id)
+    public function getAuctions($id,AuctionFilteredIndexRequest $request)
     {
+        $filter = $request->input('filter');
         $user = User::findOrFail($id);
-        $auctions = $user->Auctions()->paginate(20);
-        return response()->json($auctions);
+        $auctions = $user->Auctions();
+        if($filter == "ongoing"){
+            $auctions = $auctions->where('end_at', '>', now());
+        }elseif($filter == "ended"){
+            $auctions = $auctions->where('end_at', '<', now());
+        } elseif($filter == "won"){
+            $auctions = Auction::whereHas('offers', function($query) use ($user) {
+                $query->where('author_id', $user->id)
+                    ->whereRaw('offers.id = (SELECT MAX(o.id) FROM offers o WHERE o.auction_id = auctions.id)');
+            })->where('end_at', '<', now());
+        }
+        return response()->json($auctions->paginate(20));
     }
 
     public function getOffers($id)
